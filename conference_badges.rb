36f9059e223a9b2f00c79c189929443db70d7c1f
Jonah Brown <jswish02@gmail.com>
2019-09-16 00:08:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,22 +1,2 @@
-def badge_maker(name)
-   name ="Arel"
-  "Hello, my name is #{name}."
- end 
- def batch_badge_creator(attendees)
-  attendees ="Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"
-  attendees.each do |newnames|
-"Hello, my name is #{attendees}."
-end
-end
-
-
-
-
-
-
-def batch_badge_creator(name)
-  name = "Johnny"
-  "Hello, my name is #{name}."
-end
 
   
